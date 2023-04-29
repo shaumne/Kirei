d05ef8e93c975be61f307b888eb59d5fb993b8b3
shaumne <emrecanncetinkaya@gmail.com>
2023-04-29 08:15:14 +0300

    design ve click_elements fonksiyonu değişti

--- soundlouc.py
+++ soundlouc.py
@@ -23,38 +23,30 @@ class ClickerApp:
 
     def create_first_screen(self):
         self.root.title("Clicker App")
-        tk.Label(self.root, text="Click count:").grid(row=0, column=0, padx=10, pady=10)
-        self.click_count_entry = tk.Entry(self.root)
-        self.click_count_entry.grid(row=0, column=1, padx=10, pady=10)
-        ok_button = tk.Button(self.root, text="OK", command=self.create_second_screen)
-        ok_button.grid(row=1, column=0, padx=10, pady=10)
-
-    def create_second_screen(self):
-        click_count = int(self.click_count_entry.get())
-
-        second_screen = tk.Toplevel(self.root)
-        second_screen.title("Clicker App - Second Screen")
-
-        tk.Label(second_screen, text="URL:").grid(row=0, column=0, padx=10, pady=10)
-        self.url_entry = tk.Entry(second_screen)
+        tk.Label(self.root, text="URL:").grid(row=0, column=0, padx=10, pady=10)
+        self.url_entry = tk.Entry(self.root)
         self.url_entry.grid(row=0, column=1, padx=10, pady=10)
 
-        tk.Label(second_screen, text="Number of Threads:").grid(row=1, column=0, padx=10, pady=10)
-        self.thread_entry = tk.Entry(second_screen)
+        tk.Label(self.root, text="Number of Threads:").grid(row=1, column=0, padx=10, pady=10)
+        self.thread_entry = tk.Entry(self.root)
         self.thread_entry.grid(row=1, column=1, padx=10, pady=10)
 
-        tk.Label(second_screen, text="Number of Repeats:").grid(row=2, column=0, padx=10, pady=10)
-        self.repeat_entry = tk.Entry(second_screen)
+        tk.Label(self.root, text="Number of Repeats:").grid(row=2, column=0, padx=10, pady=10)
+        self.repeat_entry = tk.Entry(self.root)
         self.repeat_entry.grid(row=2, column=1, padx=10, pady=10)
 
-        for i in range(click_count):
-            tk.Label(second_screen, text=f"Click {i+1}:").grid(row=i+3, column=0, padx=10, pady=10)
-            xpath_entry = tk.Entry(second_screen)
-            xpath_entry.grid(row=i+3, column=1, padx=10, pady=10)
-            self.xpath_entries.append(xpath_entry)
+        self.soundcloud_var = tk.BooleanVar()
+        self.soundcloud_checkbutton = tk.Checkbutton(self.root, text="Soundcloud", variable=self.soundcloud_var)
+        self.soundcloud_checkbutton.grid(row=3, column=0, padx=10, pady=10)
 
-        start_button = tk.Button(second_screen, text="Start", command=self.start_clicking)
-        start_button.grid(row=click_count+4, column=0, padx=10, pady=10)
+        self.beatstars_var = tk.BooleanVar()
+        self.beatstars_checkbutton = tk.Checkbutton(self.root, text="Beatstars", variable=self.beatstars_var)
+        self.beatstars_checkbutton.grid(row=3, column=1, padx=10, pady=10)
+
+        self.start_button = tk.Button(self.root, text="Start", command=self.start_clicking)
+        self.start_button.grid(row=4, column=0, padx=10, pady=10)
+
+    
 
 
     def start_clicking(self):
@@ -76,21 +68,34 @@ class ClickerApp:
                 chrome_options.add_argument("--headless")
                 driver = webdriver.Chrome()
                 driver.get(url)
-                for entry in self.xpath_entries:
+                print(self.soundcloud_var.get())
+                print(self.beatstars_var.get())
+               
+                if self.soundcloud_var.get():
                     try:
-                        xpath = entry.get()
-                        print("beklenmeye başlandı")
-                        time.sleep(2)
-                        element = driver.find_element(By.XPATH, xpath)
-                        element.click()
-                        print("tıklandı")
-
+                        time.sleep(6)
+                        soundcloud_button = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div/div[2]/div/button[1]")
+                        soundcloud_button.click()
+                        time.sleep(6)
+                        soundcloud_play_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div/div[1]/a")
+                        soundcloud_play_button.click()
                     except Exception as e:
                         print(f"Error while clicking: {e}")
                         print("tıklanmadı")
-                time.sleep(9)
-
+                elif self.beatstars_var.get():
+                    try:
+                        time.sleep(6)
+                        beatstars_button = driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div/div[1]/div/div[2]/div/button[3]")
+                        beatstars_button.click()
+                        time.sleep(3)
+                        beatstars_play_button = driver.find_element(By.XPATH, "/html/body/mp-root/div/div/ng-component/mp-wrapper-member-track-content/mp-member-content-item-template/bs-container-grid/div[2]/div[2]/mp-button-play-track-visual-eq-related/div/div[1]/bs-vb-button-play-item/button/i")
+                        beatstars_play_button.click()
+                    except Exception as e:
+                        print(f"Error while clicking: {e}")
+                        print("tıklanmadı")
                 print("kapoandı")
+                time.sleep(6)
+
                 driver.quit()
                 time.sleep(1)
 
